c857e22d937d231fdadc6b38999f96fd49c1a114
aelt95 <anthonylopez1230@gmail.com>
2024-02-02 01:09:04 +0100

    mobile nav

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { SignedIn, UserButton } from "@clerk/nextjs";
 import Theme from "./Theme";
+import MobileNav from "./MobileNav";
 
 const Navbar = () => {
   return (
@@ -29,6 +30,7 @@ const Navbar = () => {
             }}
           />
         </SignedIn>
+        <MobileNav />
       </div>
     </nav>
   );
--- components/shared/navbar/Theme.tsx
+++ components/shared/navbar/Theme.tsx
@@ -5,8 +5,6 @@ import {
   MenubarContent,
   MenubarItem,
   MenubarMenu,
-  MenubarSeparator,
-  MenubarShortcut,
   MenubarTrigger,
 } from "@/components/ui/menubar";
 import { useTheme } from "@/context/ThemeProvider";
@@ -41,7 +39,18 @@ const Theme = () => {
         <MenubarContent className="absolute right-[-3rem] mt-3 min-w-[120px] rounded border py-2 dark:border-dark-400 dark:bg-dark-300">
           {themes.map((item) => {
             return (
-              <MenubarItem key={item.value}>
+              <MenubarItem
+                key={item.value}
+                className="flex items-center gap-4 px-2.5 py-2 dark:focus:bg-dark-400"
+                onClick={() => {
+                  setMode(item.value);
+                  if (item.value !== "system") {
+                    localStorage.theme = item.value;
+                  } else {
+                    localStorage.removeItem("theme");
+                  }
+                }}
+              >
                 <Image
                   src={item.icon}
                   alt={item.value}
@@ -49,17 +58,18 @@ const Theme = () => {
                   height={16}
                   className={`${mode === item.value && "active-theme"}`}
                 />
+                <p
+                  className={`body-semibold text-light-500 ${
+                    mode === item.value
+                      ? "text-primary-500"
+                      : "text-dark100_light900"
+                  }`}
+                >
+                  {item.label}
+                </p>
               </MenubarItem>
             );
           })}
-          {/* <MenubarItem>
-            New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-          </MenubarItem>
-          <MenubarItem>New Window</MenubarItem>
-          <MenubarSeparator />
-          <MenubarItem>Share</MenubarItem>
-          <MenubarSeparator />
-          <MenubarItem>Print</MenubarItem> */}
         </MenubarContent>
       </MenubarMenu>
     </Menubar>
