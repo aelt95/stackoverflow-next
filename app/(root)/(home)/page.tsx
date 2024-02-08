e50e5c64642379bac954815a3cbf847ad64dce1b
aelt95 <anthonylopez1230@gmail.com>
2024-02-08 20:59:08 +0100

    Homepage branch

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,13 @@
+import Link from "next/link";
+
 const Home = () => {
-  return <div className="h-screen"></div>;
+  return (
+    <>
+      <div>
+        <h1>All Questions</h1>
+      </div>
+    </>
+  );
 };
 
 export default Home;
