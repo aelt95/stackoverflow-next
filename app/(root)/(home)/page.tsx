e7a85d1ae03cfedceb3fc9a64fb7590743e6c0e2
aelt95 <anthonylopez1230@gmail.com>
2024-02-08 21:34:27 +0100

    localSearch homepage

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,26 @@
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import LocalSearch from "@/components/shared/search/LocalSearch";
 
 const Home = () => {
   return (
     <>
-      <div>
-        <h1>All Questions</h1>
+      <div className="flex w-full flex-col-reverse justify-between sm:flex-row">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Link href={"/ask-question"} className="flex justify-end max-sm:w-full">
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+            Ask a Question
+          </Button>
+        </Link>
+      </div>
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearch
+          route="/"
+          iconPosition="left"
+          imgSrc="/aassets/icons/search.svg"
+          placeholder="Search for questions"
+          otherClasses="flex-1"
+        />
       </div>
     </>
   );
