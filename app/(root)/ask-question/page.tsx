5a6adfa4b24c1617ff41ad6c9eba41171f32df09
aelt95 <anthonylopez1230@gmail.com>
2024-03-23 11:36:00 +0100

    ask a question page initial implementation

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Question from "@/components/forms/Question";
 
 const AskQuestion = () => {
-  return <div>Ask Question</div>;
+  return (
+    <div>
+      <div>
+        <Question />
+      </div>
+    </div>
+  );
 };
 
 export default AskQuestion;
