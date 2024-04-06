e5fb8454e82e3d7e246af0597ad96ab3e62f3f89
aelt95 <anthonylopez1230@gmail.com>
2024-04-06 20:13:12 +0200

    database actions

---
+++ database/tag.model.ts
@@ -0,0 +1,20 @@
+import { Schema, models, model, Document } from "mongoose";
+
+export interface ITag extends Document {
+  name: string;
+  description: string;
+  question: Schema.Types.ObjectId[];
+  followers: Schema.Types.ObjectId[];
+  createdOn: Date;
+}
+
+const TagSchema = new Schema({
+  name: { type: String, required: true, unique: true },
+  description: { type: String, required: true },
+  questions: [{ type: Schema.Types.ObjectId, ref: "Question" }],
+  followers: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  createdOn: { type: Date, default: Date.now },
+});
+
+const Tag = models.User || model("User", TagSchema);
+export default Tag;
