e46db6d5be3d672c3f7d43f6b2e1d5aafee1c0a1
aelt95 <anthonylopez1230@gmail.com>
2024-04-07 16:47:08 +0200

    QuestionCards edited, homepage fetching data

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -1,9 +1,12 @@
 "use server";
 import Question from "@/database/question.model";
 import Tag from "@/database/tag.model";
+import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
+import { GetQuestionsParams, CreateQuestionParams } from "./shared.types";
 
-export async function createQuestion(params: any) {
+//Create a Question
+export async function createQuestion(params: CreateQuestionParams) {
   try {
     connectToDatabase();
     const { title, content, tags, author, path } = params;
@@ -27,6 +30,19 @@ export async function createQuestion(params: any) {
     await Question.findByIdAndUpdate(question._id, {
       $push: { tags: { $each: tagDocuments } },
     });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//Fetch Questions
+export async function getQuestions(params: GetQuestionsParams) {
+  try {
+    connectToDatabase();
+    const questions = await Question.find({})
+      .populate({ path: "tags", model: Tag })
+      .populate({ path: "author", model: User });
+    return { questions };
   } catch (error) {
     console.log(error);
   }
