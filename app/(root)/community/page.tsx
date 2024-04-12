1bbdeda589561e7e8f143f0e43fa9377d11e88db
aelt95 <anthonylopez1230@gmail.com>
2024-04-12 15:06:01 +0200

    Community page

---
+++ app/(root)/community/page.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import LocalSearch from "@/components/shared/search/LocalSearch";
+import Filter from "@/components/shared/search/Filter";
+import { UserFilters } from "@/constants/filters";
+import { getAllUser } from "@/lib/actions/user.actions";
+import Link from "next/link";
+import UserCard from "@/components/shared/cards/UserCard";
+
+const Page = async () => {
+  const usersResult = await getAllUser({});
+
+  return (
+    <>
+      <div className="flex w-full flex-col-reverse justify-between sm:flex-row">
+        <h1 className="h1-bold text-dark100_light900">All Users</h1>
+      </div>
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearch
+          route="/community"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for amazing minds"
+          otherClasses="flex-1"
+        />
+        <Filter
+          filters={UserFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+        />
+      </div>
+      <section className="mt-12 flex flex-wrap gap-4">
+        {usersResult.users.length > 0 ? (
+          usersResult.users.map((user) => {
+            return <UserCard key={user._id} user={user} />;
+          })
+        ) : (
+          <div className="paragraph-regular text-dark200_light800 mx-auto max-w-4xl text-center">
+            <p>No users yet</p>
+            <Link href={"/sing-up"} className="mt-3 font-bold text-accent-blue">
+              Join us now!
+            </Link>
+          </div>
+        )}
+      </section>
+    </>
+  );
+};
+
+export default Page;
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,6 +5,7 @@ import { connectToDatabase } from "../mongoose";
 import {
   CreateUserParams,
   DeleteUserParams,
+  GetAllUsersParams,
   UpdateUserParams,
 } from "./shared.types";
 import { revalidatePath } from "next/cache";
@@ -59,5 +60,17 @@ export async function deleteUser(params: DeleteUserParams) {
     return deletedUser;
   } catch (error) {
     console.log(error);
+  }
+}
+
+export async function getAllUser(params: GetAllUsersParams) {
+  try {
+    connectToDatabase();
+    // const { page = 1, pageSize = 20, filter, searchQuery } = params;
+    const users = await User.find({}).sort({ createdAt: -1 });
+    return { users };
+  } catch (error) {
+    console.log(error);
+    throw error;
   }
 }
