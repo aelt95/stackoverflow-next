b65d4fcff1d63067f6bc61f02e9d58ee8f791e92
aelt95 <anthonylopez1230@gmail.com>
2024-04-12 22:18:57 +0200

    retrieve user function fixed

--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -8,10 +8,12 @@ import User from "@/database/user.model";
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
     connectToDatabase();
-    const { userId, limit = 3 } = params;
-    const user = await User.find(userId);
+    const { userId } = params;
+    const user = await User.findById(userId);
 
     if (!user) throw new Error("User not found");
+
+    return ["tag1", "tag2", "tag3"];
   } catch (error) {
     console.log(error);
     throw error;
