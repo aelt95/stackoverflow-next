832ee132047ca15b9c342f3ee70b62e52148103b
aelt95 <anthonylopez1230@gmail.com>
2024-04-15 22:48:34 +0200

    Metrics & styles

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -2,6 +2,9 @@ import { getQuestionById } from "@/lib/actions/question.action";
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import Metric from "@/components/shared/Metric";
+import { getTimeStamp } from "@/lib/utils";
+import ParseHTML from "@/components/shared/ParseHTML";
 
 const Page = async ({ params, searchParams }) => {
   const questionResult = await getQuestionById({ questionId: params.id });
@@ -10,8 +13,11 @@ const Page = async ({ params, searchParams }) => {
   return (
     <>
       <div className="flex-start w-full flex-col">
-        <div>
-          <Link href={`/profile/${questionResult.author.clerkId}`}>
+        <div className="flex w-full flex-col-reverse justify-between gap-5 sm:flex-row sm:items-centert sm:gap-2">
+          <Link
+            href={`/profile/${questionResult.author.clerkId}`}
+            className="flex items-center justify-start gap-1"
+          >
             <Image
               src={questionResult.author.picture}
               height={22}
@@ -19,11 +25,40 @@ const Page = async ({ params, searchParams }) => {
               alt="Profile picture"
               className="rounded-full"
             ></Image>
-            <p>{questionResult.author.name}</p>
+            <p className="paragraph-semibold text-dark300_light700">
+              {questionResult.author.name}
+            </p>
           </Link>
+          <div className="flex justify-end">VOTING</div>
         </div>
+        <h2 className="h2-semibold text-dark200_light900 mt-3.5 w-full text-left">
+          {questionResult.title}
+        </h2>
       </div>
-      {questionResult.title}
+      <div className="mb-8 mt-5 flex flex-wrap gap-4">
+        <Metric
+          imgUrl="/assets/icons/clock.svg"
+          alt="clock icon"
+          value={`asked ${getTimeStamp(questionResult.createdAt)}`}
+          title="Asked"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/message.svg"
+          alt="Message"
+          value={questionResult.answers}
+          title="Answers"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/eye.svg"
+          alt="eye"
+          value={questionResult.views}
+          title="Views"
+          textStyles="small-medium text-dark400_light800"
+        />
+      </div>
+      <ParseHTML data={questionResult.content} />
     </>
   );
 };
