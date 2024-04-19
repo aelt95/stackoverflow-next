90d8558a8ab82efbfa7ccb7b6d01dc4ce540f307
aelt95 <anthonylopez1230@gmail.com>
2024-04-19 23:25:13 +0200

    Initial Votes.tsx structure

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -1,7 +1,45 @@
+import Image from "next/image";
 import React from "react";
-
-const Votes = () => {
-  return <div>Votes</div>;
+interface Props {
+  type: string;
+  itemId: string;
+  userId: string;
+  upvotes: number;
+  downvotes: number;
+  hasupVoted: boolean;
+  hasdownVoted: boolean;
+  hasSaved: boolean;
+}
+const Votes = ({
+  type,
+  itemId,
+  userId,
+  upvotes,
+  downvotes,
+  hasupVoted,
+  hasdownVoted,
+  hasSaved,
+}: Props) => {
+  return (
+    <div className="flex gap-5">
+      <div className="flex-center gap-2.5">
+        <div className="flex-center gap-1.5">
+          <Image
+            src={
+              hasupVoted
+                ? "/assets/icons/upvoted.svg"
+                : "/assets/icons/upvote.svg"
+            }
+            width={18}
+            height={18}
+            alt="upvote"
+            className="cursor-pointer"
+            onClick={() => {}}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Votes;
