158b3226c5d77e251cec23247aab724ddca63c11
aelt95 <anthonylopez1230@gmail.com>
2024-05-01 00:36:08 +0200

    interaction model finished

--- database/interaction.model.ts
+++ database/interaction.model.ts
@@ -3,8 +3,8 @@ import { Schema, model, models, Document } from "mongoose";
 export interface Iinteraction extends Document {
   user: Schema.Types.ObjectId;
   action: string;
-  questions: Schema.Types.ObjectId[];
-  answers: Schema.Types.ObjectId[];
+  questions: Schema.Types.ObjectId;
+  answers: Schema.Types.ObjectId;
   tags: Schema.Types.ObjectId[];
   createdAt: Date;
 }
@@ -12,8 +12,9 @@ export interface Iinteraction extends Document {
 const InteractionSchema = new Schema({
   user: [{ type: Schema.Types.ObjectId, ref: "User", required: true }],
   action: { type: String, required: true },
-  description: { type: String, required: true },
-  questions: [{ type: Schema.Types.ObjectId, ref: "Question" }],
+  questions: { type: Schema.Types.ObjectId, ref: "Question" },
+  answers: { type: Schema.Types.ObjectId, ref: "Answer" },
+  tags: [{ type: Schema.Types.ObjectId, ref: "Answer" }],
   createdOn: { type: Date, default: Date.now },
 });
 
