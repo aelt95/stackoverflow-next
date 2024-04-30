3f92bd0d37dc7badebdd42de93ea40f591dbb805
aelt95 <anthonylopez1230@gmail.com>
2024-05-01 00:27:48 +0200

    interaction model & action

---
+++ database/interaction.model.ts
@@ -0,0 +1,23 @@
+import { Schema, model, models, Document } from "mongoose";
+
+export interface Iinteraction extends Document {
+  user: Schema.Types.ObjectId;
+  action: string;
+  questions: Schema.Types.ObjectId[];
+  answers: Schema.Types.ObjectId[];
+  tags: Schema.Types.ObjectId[];
+  createdAt: Date;
+}
+
+const InteractionSchema = new Schema({
+  user: [{ type: Schema.Types.ObjectId, ref: "User", required: true }],
+  action: { type: String, required: true },
+  description: { type: String, required: true },
+  questions: [{ type: Schema.Types.ObjectId, ref: "Question" }],
+  createdOn: { type: Date, default: Date.now },
+});
+
+const Interaction =
+  models.Interaction || model("Interaction", InteractionSchema);
+
+export default Interaction;
