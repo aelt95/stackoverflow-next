8d5405bfdbe501edcad6c24e53a522276bd5240e
aelt95 <anthonylopez1230@gmail.com>
2024-05-02 22:50:59 +0200

    tag detail bug

--- app/(root)/tags/[id]/page.tsx
+++ app/(root)/tags/[id]/page.tsx
@@ -1,14 +1,62 @@
+import NoResult from "@/components/shared/NoResult";
+import QuestionCard from "@/components/shared/cards/QuestionCard";
+import Filter from "@/components/shared/search/Filter";
+import LocalSearch from "@/components/shared/search/LocalSearch";
 import { getQuestionsByTagId } from "@/lib/actions/tag.actions";
 import React from "react";
 
 const Page = async ({ params, searchParams }: any) => {
-  console.log(searchParams);
-  const tags = await getQuestionsByTagId({
+  const result = await getQuestionsByTagId({
     tagId: params.id,
     page: 1,
     searchQuery: searchParams.p,
   });
-  return <div>page</div>;
+
+  console.log(result);
+  return (
+    <>
+      <>
+        <h1 className="h1-bold text-dark100_light900">{`You are looking at ${result.tagTitle} results`}</h1>
+      </>
+      <div className="mt-11 w-full">
+        <LocalSearch
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions"
+          otherClasses="flex-1"
+        />
+      </div>
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {result?.questions && result.questions.length > 0 ? (
+          result.questions.map((question: any) => {
+            return (
+              <QuestionCard
+                key={question._id}
+                _id={question._id}
+                title={question.title}
+                tags={question.tags}
+                author={question.author}
+                upvotes={question.upvotes}
+                views={question.views}
+                answers={question.answers}
+                createdAt={question.createdAt}
+              />
+            );
+          })
+        ) : (
+          <NoResult
+            title={"There's no tag saved to show"}
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the
+        discussion. our query could be the next big thing others learn from. Get
+        involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
+    </>
+  );
 };
 
 export default Page;
