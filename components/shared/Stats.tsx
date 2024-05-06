37cf47fc6f1b45ceba41feb8850834b83f6c71d0
aelt95 <anthonylopez1230@gmail.com>
2024-05-06 21:11:25 +0200

    Badge UI implemented

--- components/shared/Stats.tsx
+++ components/shared/Stats.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import Image from "next/image";
 
 interface Props {
   totalAnswers: number;
   totalQuestions: number;
 }
+
+interface StatsCardProps {
+  imgUrl: string;
+  value: number;
+  title: string;
+}
+
+const StatsCard = ({ imgUrl, value, title }: StatsCardProps) => {
+  return (
+    <div className="light-border background-light900_dark300 flex flex-wrap items-center justify-evenly gap-4 rounded-md border p-6 shadow-light-300 dark:shadow-dark-200">
+      <Image src={imgUrl} width={40} height={50} alt="badge" />
+      <div>
+        <p className="paragraph-semibold text-dark200_light900">{value}</p>
+        <p className="body-medium text-dark400_light700">{title}</p>
+      </div>
+    </div>
+  );
+};
 
 const Stats = ({ totalAnswers, totalQuestions }: Props) => {
   return (
@@ -24,6 +42,21 @@ const Stats = ({ totalAnswers, totalQuestions }: Props) => {
             <p className="body-medium text-dark400_light700">Answers</p>
           </div>
         </div>
+        <StatsCard
+          imgUrl="/assets/icons/gold-medal.svg"
+          value={0}
+          title="Gold Badges"
+        />
+        <StatsCard
+          imgUrl="/assets/icons/silver-medal.svg"
+          value={0}
+          title="silver Badges"
+        />
+        <StatsCard
+          imgUrl="/assets/icons/bronze-medal.svg"
+          value={0}
+          title="bronze Badges"
+        />
       </div>
     </div>
   );
