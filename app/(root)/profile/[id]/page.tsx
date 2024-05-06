3357c0a8d42162b6415565fb2c74713c48cab30b
aelt95 <anthonylopez1230@gmail.com>
2024-05-06 21:41:10 +0200

    Top post used

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -90,11 +90,11 @@ const Page = async ({ params, searchParams }: URLProps) => {
             </TabsTrigger>
           </TabsList>
           <TabsContent value="top-posts">
-            {/* <QuestionTab
+            <QuestionTab
               searchParams={searchParams}
               userId={userInfo.user._id}
               clerkId={clerkId}
-            /> */}
+            />
           </TabsContent>
           <TabsContent value="answers" className="flex w-full flex-col gap-6">
             {/* <AnswersTab
--- components/shared/QuestionTab.tsx
+++ components/shared/QuestionTab.tsx
@@ -1,7 +1,36 @@
+import { getUserQuestions } from "@/lib/actions/user.actions";
+import { SearchParamsProps } from "@/types";
 import React from "react";
+import QuestionCard from "./cards/QuestionCard";
 
-const QuestionTab = () => {
-  return <div>QuestionTab</div>;
+interface Props extends SearchParamsProps {
+  userId: string;
+  clerkId?: string;
+}
+
+const QuestionTab = async ({ searchProps, userId, clerkId }: Props) => {
+  const result = await getUserQuestions({ userId, page: 1 });
+
+  return (
+    <>
+      {result.questions.map((item) => {
+        return (
+          <QuestionCard
+            key={item._id}
+            _id={item._id}
+            clerkId={clerkId}
+            title={item.title}
+            tags={item.tags}
+            author={item.author}
+            upvotes={item.upvotes}
+            views={item.views}
+            answers={item.answers}
+            createdAt={item.createdAt}
+          />
+        );
+      })}
+    </>
+  );
 };
 
 export default QuestionTab;
