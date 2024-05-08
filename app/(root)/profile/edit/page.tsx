1474e8249d8d31a3f05c76c0ea1c5db09b9f172e
aelt95 <anthonylopez1230@gmail.com>
2024-05-08 21:13:20 +0200

    edit profile form

--- app/(root)/profile/edit/page.tsx
+++ app/(root)/profile/edit/page.tsx
@@ -5,16 +5,15 @@ import { getUserById } from "@/lib/actions/user.actions";
 import { auth } from "@clerk/nextjs";
 import React from "react";
 
-const Page = async ({ params }: any) => {
+const Page = async () => {
+  const { userId } = auth();
+  if (!userId) return null;
+  const mongoUser = await getUserById({ userId });
   return (
     <>
       <h1 className="h1-bold text-dark100_light900">Edit Profile</h1>
       <div className="mt-9">
-        <Profile
-          type="Edit"
-          mongouserId={""}
-          questionDetails={JSON.stringify("")}
-        />
+        <Profile clerkId={userId} user={JSON.stringify(mongoUser)} />
       </div>
     </>
   );
--- components/forms/Profile.tsx
+++ components/forms/Profile.tsx
@@ -1,7 +1,66 @@
+"use client";
 import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
-const Profile = (props: any) => {
-  return <div>Profile</div>;
+interface Props {
+  clerkId: string;
+  user: string;
+}
+const Profile = ({ clerkId, user }: Props) => {
+  const parsedUser = JSON.parse(user);
+
+  const formSchema = z.object({
+    username: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
+    }),
+  });
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 };
 
 export default Profile;
