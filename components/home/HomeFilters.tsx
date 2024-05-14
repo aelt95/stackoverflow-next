7289c12e8115e92e59bbf4a5049cc010627a31bb
aelt95 <anthonylopez1230@gmail.com>
2024-05-14 12:03:53 +0200

    Filter functionality

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -1,17 +1,57 @@
-"use client"
-import { HomePageFilters } from "@/constants/filters"
-import { Button } from "../ui/button"
+"use client";
+import { HomePageFilters } from "@/constants/filters";
+import { Button } from "../ui/button";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { formUrlQuery } from "@/lib/utils";
 
 const HomeFilters = () => {
-  const active = "newest";
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const [active, setActive] = useState("second");
+
+  const handleTypeClick = (item: string) => {
+    if (active !== item) {
+      setActive(item);
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: item.toLocaleLowerCase(),
+      });
+      router.push(newUrl, { scroll: false });
+    } else {
+      setActive("");
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: null,
+      });
+      router.push(newUrl, { scroll: false });
+    }
+  };
+  useEffect(() => {}, []);
 
   return (
     <div className="mt-10 flex-wrap gap-3 md:flex hidden">
-      {HomePageFilters.map(item => {
-      return <Button key={item.value} onClick={()=>{}} className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none ${active === item.value ? "bg-primary-100 text-primary-500" : "dark:text-light-500 bg-light-800 text-light-500 hover:bg-light-700 dark:bg-dark-300 dark:hover:bg-dark-400" }`}>{item.name}</Button>
-    })}
+      {HomePageFilters.map((item) => {
+        return (
+          <Button
+            key={item.value}
+            onClick={() => {
+              handleTypeClick(item.value);
+            }}
+            className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none ${
+              active === item.value
+                ? "bg-primary-100 text-primary-500"
+                : "dark:text-light-500 bg-light-800 text-light-500 hover:bg-light-700 dark:bg-dark-300 dark:hover:bg-dark-400"
+            }`}
+          >
+            {item.name}
+          </Button>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default HomeFilters
+export default HomeFilters;
