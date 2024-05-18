b7fbb5d6bc555c51f198f466bb014fa393c79654
aelt95 <anthonylopez1230@gmail.com>
2024-05-18 13:13:16 +0200

    Loading for community page

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import UserCard from "@/components/shared/cards/UserCard";
 import { SearchParamsProps } from "@/types";
 import Pagination from "@/components/shared/Pagination";
+import Loading from "./loading";
 
 const Page = async ({ searchParams }: SearchParamsProps) => {
   const usersResult = await getAllUser({
